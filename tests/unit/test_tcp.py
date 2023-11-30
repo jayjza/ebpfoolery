c9083352ea54a7e96c0acfa83968298e42319867
Francois <francoistk@gmail.com>
2023-11-30 07:58:37 +0000

    Added tests for T2-T7

--- tests/unit/test_tcp.py
+++ tests/unit/test_tcp.py
@@ -205,3 +205,195 @@ def test_nmap_sequence_generation(device_under_test, window_field, tcp_options, 
 
     for field, value in response['TCP'].items():
         assert resp[TCP].getfieldval(field) == value
+
+def test_nmap_T2(device_under_test):
+    """
+    Tests NMAP T2 response.
+    T2 sends a TCP null (no flags set) packet with the IP DF bit set and a window field of 128 to an open port.
+        window scale (10), NOP, MSS (265), Timestamp (TSval: 0xFFFFFFFF; TSecr: 0), then SACK permitted
+    """
+    tcp_options = [
+            ('WScale', 10),
+            ('NOP', b''),
+            ('MSS', struct.pack('>H', 265)),
+            ('Timestamp', (4294967295, 0)),
+            ('SAckOK', b''),
+    ]
+
+    tcp_probe_packet = (
+        Ether() /
+        IP (dst=str(device_under_test.IP),
+            flags="DF") /
+        TCP(sport=RandShort(),
+            dport=12345,
+            flags=0,
+            window=128,
+            options=tcp_options)
+    )
+
+    print("Sending: {}".format(repr(tcp_probe_packet)))
+    resp = srp1(tcp_probe_packet, iface=device_under_test.interface, timeout=1)
+    print(repr(resp))
+
+    assert resp is None, "Excpected no response but received {}".format(repr(resp))
+
+def test_nmap_T3(device_under_test):
+    """
+    Tests NMAP T3 response.
+    T3 sends a TCP packet with the SYN, FIN, URG, and PSH flags set and a window field of 256 to an open port. The IP DF bit is not set.
+        window scale (10), NOP, MSS (265), Timestamp (TSval: 0xFFFFFFFF; TSecr: 0), then SACK permitted
+    """
+    tcp_options = [
+            ('WScale', 10),
+            ('NOP', b''),
+            ('MSS', struct.pack('>H', 265)),
+            ('Timestamp', (4294967295, 0)),
+            ('SAckOK', b''),
+    ]
+
+    tcp_probe_packet = (
+        Ether() /
+        IP (dst=str(device_under_test.IP)) /
+        TCP(sport=RandShort(),
+            dport=12345,
+            flags="SFUP",
+            window=256,
+            options=tcp_options)
+    )
+
+    print("Sending: {}".format(repr(tcp_probe_packet)))
+    resp = srp1(tcp_probe_packet, iface=device_under_test.interface, timeout=1)
+    print(repr(resp))
+
+    assert resp is None, "Excpected no response but received {}".format(repr(resp))
+
+def test_nmap_T4(device_under_test):
+    """
+    Tests NMAP T4 response.
+    T4 sends a TCP ACK packet with IP DF and a window field of 1024 to an open port.
+        window scale (10), NOP, MSS (265), Timestamp (TSval: 0xFFFFFFFF; TSecr: 0), then SACK permitted
+    """
+    tcp_options = [
+            ('WScale', 10),
+            ('NOP', b''),
+            ('MSS', struct.pack('>H', 265)),
+            ('Timestamp', (4294967295, 0)),
+            ('SAckOK', b''),
+    ]
+
+    tcp_probe_packet = (
+        Ether() /
+        IP (dst=str(device_under_test.IP),
+            flags="DF") /
+        TCP(sport=RandShort(),
+            dport=12345,
+            flags='A',
+            window=1024,
+            options=tcp_options)
+    )
+
+    print("Sending: {}".format(repr(tcp_probe_packet)))
+    resp = srp1(tcp_probe_packet, iface=device_under_test.interface, timeout=1)
+    print(repr(resp))
+
+    assert resp is None, "Excpected no response but received {}".format(repr(resp))
+
+def test_nmap_T5(device_under_test):
+    """
+    Tests NMAP T5 response.
+    T5 sends a TCP SYN packet without IP DF and a window field of 31337 to a closed port.
+        window scale (10), NOP, MSS (265), Timestamp (TSval: 0xFFFFFFFF; TSecr: 0), then SACK permitted
+    """
+    tcp_options = [
+            ('WScale', 10),
+            ('NOP', b''),
+            ('MSS', struct.pack('>H', 265)),
+            ('Timestamp', (4294967295, 0)),
+            ('SAckOK', b''),
+    ]
+
+    tcp_probe_packet = (
+        Ether() /
+        IP (dst=str(device_under_test.IP)) /
+        TCP(sport=RandShort(),
+            dport=12345,
+            flags='A',
+            window=31337,
+            options=tcp_options)
+    )
+
+    print("Sending: {}".format(repr(tcp_probe_packet)))
+    resp = srp1(tcp_probe_packet, iface=device_under_test.interface, timeout=1)
+    print(repr(resp))
+
+    assert IP in resp, "No IP layer found in response"
+    assert TCP in resp, "No TCP layer found in response"
+
+    assert resp[IP].ttl == 128, "Incorrect TTL"
+    assert resp[IP].flags == "DF", "Incorrect IP Flags"
+    assert resp[TCP].window == 0, "Incorrect TCP Window"
+    assert resp[TCP].seq == 0, "Incorrect TCP Sequence"
+    assert resp[TCP].flags == "RA", "Incorect TCP Flags"
+
+def test_nmap_T6(device_under_test):
+    """
+    Tests NMAP T6 response.
+    T6 sends a TCP ACK packet with IP DF and a window field of 32768 to a closed port.
+
+
+        window scale (10), NOP, MSS (265), Timestamp (TSval: 0xFFFFFFFF; TSecr: 0), then SACK permitted
+    """
+    tcp_options = [
+            ('WScale', 10),
+            ('NOP', b''),
+            ('MSS', struct.pack('>H', 265)),
+            ('Timestamp', (4294967295, 0)),
+            ('SAckOK', b''),
+    ]
+
+    tcp_probe_packet = (
+        Ether() /
+        IP (dst=str(device_under_test.IP),
+            flags="DF") /
+        TCP(sport=RandShort(),
+            dport=12345,
+            flags='A',
+            window=32768,
+            options=tcp_options)
+    )
+
+    print("Sending: {}".format(repr(tcp_probe_packet)))
+    resp = srp1(tcp_probe_packet, iface=device_under_test.interface, timeout=1)
+    print(repr(resp))
+
+    assert resp is None, "Excpected no response but received {}".format(repr(resp))
+
+def test_nmap_T7(device_under_test):
+    """
+    Tests NMAP T7 response.
+    T7 sends a TCP packet with the FIN, PSH, and URG flags set and a window field of 65535 to a closed port. The IP DF bit is not set.
+        window scale (10), NOP, MSS (265), Timestamp (TSval: 0xFFFFFFFF; TSecr: 0), then SACK permitted
+    """
+    tcp_options = [
+            ('WScale', 15),
+            ('NOP', b''),
+            ('MSS', struct.pack('>H', 265)),
+            ('Timestamp', (4294967295, 0)),
+            ('SAckOK', b''),
+    ]
+
+    tcp_probe_packet = (
+        Ether() /
+        IP (dst=str(device_under_test.IP)) /
+        TCP(sport=RandShort(),
+            dport=12345,
+            flags='FPU',
+            window=65535,
+            options=tcp_options)
+    )
+
+    print("Sending: {}".format(repr(tcp_probe_packet)))
+    resp = srp1(tcp_probe_packet, iface=device_under_test.interface, timeout=1)
+    print(repr(resp))
+
+    assert resp is None, "Excpected no response but received {}".format(repr(resp))
