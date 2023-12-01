1b14d0b7443818ef40be9aa89d5f3afa097ba55b
Jay <bissictjj@gmail.com>
2023-12-01 13:11:04 +0000

    fix nmap test

--- tests/unit/win2016/test_nmap.py
+++ tests/unit/win2016/test_nmap.py
@@ -3,7 +3,7 @@ import nmap
 def test_nmap_OS_scan(device_under_test):
     nm = nmap.PortScanner()
     result = nm.scan(str(device_under_test.IP), arguments='-O')
-    assert result['scan']['192.168.31.158']['osmatch'][0]['osclass'][0] == {
-        'type': 'general purpose', 'vendor': 'Microsoft',
-        'osfamily': 'Windows', 'osgen': '2016', 'accuracy': '99',
-        'cpe': ['cpe:/o:microsoft:windows_server_2016']}
+    assert result['scan']['192.168.31.158']['osmatch'][0]['osclass'][0]['vendor'] == 'Microsoft'
+    assert result['scan']['192.168.31.158']['osmatch'][0]['osclass'][0]['osgen'] == '2016'
+    assert result['scan']['192.168.31.158']['osmatch'][0]['osclass'][0]['osfamily'] == 'Windows'
+    assert result['scan']['192.168.31.158']['osmatch'][0]['osclass'][0]['cpe'] == ['cpe:/o:microsoft:windows_server_2016']
